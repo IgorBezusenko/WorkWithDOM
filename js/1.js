faa41a5d6ee2f6072fc5f0e7a2075f9d1c2f50d7
IgorBezusenko <bezusenko.98@mail.ru>
2020-06-24 18:46:43 +0300

    task 1

---
+++ js/1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const head = document.head;
+console.log(head);
+
+const body = document.body;
+console.log(body);
+
+let childBody = [];
+childBody = body.children;
+console.dir(childBody);
+
+console.log("///");
+
+const div1 = body.firstElementChild;
+console.log(div1);
+
+console.log("///");
+
+let nextP = "";
+nextP = div1.firstElementChild;
+nextP = nextP.nextElementSibling;
+let otherP = "";
+otherP = div1.lastElementChild;
+otherP = otherP.previousElementSibling;
+console.log(nextP);
+console.log(otherP);
