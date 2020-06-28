2928d61414071ad2cc6366eeeba2a1173982b989
IgorBezusenko <bezusenko.98@mail.ru>
2020-06-28 14:19:58 +0300

    task 4

---
+++ js/4.js
@@ -0,0 +1,35 @@
+// "use strict";
+
+// //   1
+
+// const ul = document.querySelector("ul");
+// ul.classList.add("list");
+// console.log(ul);
+
+// //   2
+
+// const span = document.querySelector("span");
+// console.log(span);
+
+// const link = span.nextElementSibling;
+// link.setAttribute("id", "link");
+// console.log(link);
+
+// //  3
+
+// const li1 = ul.querySelector("li");
+// li1.classList.add("item");
+// console.log(li1);
+// const li4 = ul.lastElementChild;
+// const li3 = li4.previousElementSibling;
+// li3.classList.add("item");
+// console.log(li3);
+
+// //  4
+
+// const links = document.querySelectorAll("a");
+// console.log(links);
+// const allLink = links.forEach((el) => {
+//   el.classList.add("custom-link");
+// });
+// console.log(allLink);
